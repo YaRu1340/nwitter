dd41bcf3b4c52ceea63d2aa19c04efd3163b611e
YaRu1340 <bomi050704@gmail.com>
2024-08-16 15:46:01 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/App.js 	new file:   src/components/Router.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './components/App';
 import firebase from "./firebase";
 console.log(firebase);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </React.StrictMode>
 );
